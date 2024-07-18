a0a53a676e0eb16888d6e78409b5e0fcede67bae
Sameer Nadaf <nsameernadaf@gmail.com>
2024-07-18 19:07:27 +0530

    Add historyApiFallback

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,12 @@ app.use(cors());
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, '../images')));
 
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, '../images')
@@ -637,9 +643,6 @@ app.get('/cartCount', (req, res) => {
     });
 });
 
-
-
-
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on https://backend-uqfl.onrender.com`);
 });
